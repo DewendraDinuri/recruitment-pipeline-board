d272d5fa656bcf9f09b3c2986925887a9616eb12
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-05-19 01:21:22 +0530

    Pollished Client page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,22 @@
-import CandidateModal from './components/CandidateModal';
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Board from './pages/Board';
+import CandidateModal from './components/CandidateModal';
 
 function App() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
+      <Navbar onAddClick={() => setShowModal(true)} />
+      <Header />
+      <div className="App">
+        <Sidebar />
+        <Board />
+      </div>
       <CandidateModal show={showModal} onClose={() => setShowModal(false)} />
-      <>
-        <Navbar />
-        <Header />
-        <div className="App">
-          <Sidebar />
-          <Board />
-        </div>
-      </>
     </>
   );
 }
