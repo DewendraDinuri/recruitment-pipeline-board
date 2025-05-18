5b83363358cf6115576f6235542c4356077e0478
DINURI BUDHARA DEWENDRA <85922443+DewendraDinuri@users.noreply.github.com>
2025-05-19 01:12:32 +0530

    remove unessacery README file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import CandidateModal from './components/CandidateModal';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Board from './pages/Board';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
-      <Navbar />
-      <Header />
-      <div className="App">
-        <Sidebar />
-        <Board />
-      </div>
+      <CandidateModal show={showModal} onClose={() => setShowModal(false)} />
+      <>
+        <Navbar />
+        <Header />
+        <div className="App">
+          <Sidebar />
+          <Board />
+        </div>
+      </>
     </>
   );
 }
